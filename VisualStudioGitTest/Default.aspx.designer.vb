e6bb1900252d23cc66578d69cece41966131dc58
danbarlow27 <danbarlow27@gmail.com>
2013-05-23 11:18:12 -0400

    sdefijnf

--- VisualStudioGitTest/Default.aspx.designer.vb
+++ VisualStudioGitTest/Default.aspx.designer.vb
@@ -12,4 +12,13 @@ Option Explicit On
 
 
 Partial Public Class _Default
+
+    '''<summary>
+    '''Button1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
 End Class
--- VisualStudioGitTest/Default.aspx.vb
+++ VisualStudioGitTest/Default.aspx.vb
@@ -4,4 +4,8 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 
     End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
